989f6ec6dea076a98e635093244a4d0d3121be52
D-Simone <manuel.desimone30@gmail.com>
2023-05-23 07:59:45 +0200

    Update files

--- index.js
+++ index.js
@@ -1,129 +1,202 @@
 // Function to handle form submission and saving a new template
 function handleFormSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    // Get the input values from the form
-    const titleInput = document.getElementById('templateTitle');
-    const contentInput = document.getElementById('templateContent');
+  // Get the input values from the form
+  const titleInput = document.getElementById('templateTitle');
+  const contentInput = document.getElementById('templateContent');
 
-    // Validate the inputs
-    if (titleInput.value.trim() === '' || contentInput.value.trim() === '') {
-        alert('Please enter a title and content for the template.');
-        return;
-    }
+  // Create a new template object
+  const template = {
+    title: titleInput.value,
+    content: contentInput.value,
+  };
 
-    // Create a new template object
-    const template = {
-        title: titleInput.value,
-        content: contentInput.value,
-    };
-
-    // Save the template to the table and storage
+  // Save the template only if both title and content are not empty
+  if (template.title.trim() !== '' && template.content.trim() !== '') {
+    // Save the template to the storage
     saveTemplate(template);
 
-    // Clear the form inputs
+    // Reset the form inputs
     titleInput.value = '';
     contentInput.value = '';
+  }
 }
 
 // Function to copy the template content to the clipboard
 function copyTemplate(content) {
-    const textarea = document.createElement('textarea');
-    textarea.value = content;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textarea);
-    alert('Template content copied to clipboard!');
+  navigator.clipboard.writeText(content)
+    .then(() => {
+      console.log('Template content copied to clipboard:', content);
+    })
+    .catch((error) => {
+      console.error('Failed to copy template content to clipboard:', error);
+    });
 }
 
-// Function to save the template to the table and storage
+// Function to save the template to the storage
 function saveTemplate(template) {
-    // Create a new row in the table
-    const tableBody = document.querySelector('table tbody');
-    const newRow = document.createElement('tr');
+  // Add the template to the results container
+  const resultsContainer = document.getElementById('resultsContainer');
 
-    // Create table cells for the template title, content, and actions
-    const titleCell = document.createElement('td');
-    titleCell.textContent = template.title;
+  // Create the template element
+  const templateElement = document.createElement('div');
+  templateElement.classList.add('entry', 'custom'); // Add 'custom' class
 
-    const contentCell = document.createElement('td');
-    contentCell.textContent = template.content;
+  // Create the icon element
+  const iconElement = document.createElement('div');
+  iconElement.classList.add('icon');
+  iconElement.innerHTML = `<svg fill="none" viewBox="0 0 24 24" height="20" width="20">
+                              <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2" stroke="#171718" d="M13 2H13.2727C16.5339 2 18.1645 2 19.2969 2.79784C19.6214 3.02643 19.9094 3.29752 20.1523 3.60289C21 4.66867 21 6.20336 21 9.27273V11.8182C21 14.7814 21 16.2629 20.5311 17.4462C19.7772 19.3486 18.1829 20.8491 16.1616 21.5586C14.9044 22 13.3302 22 10.1818 22C8.38275 22 7.48322 22 6.76478 21.7478C5.60979 21.3424 4.69875 20.4849 4.26796 19.3979C4 18.7217 4 17.8751 4 16.1818V12"></path>
+                              <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2" stroke="#171718" d="M21 12C21 13.8409 19.5076 15.3333 17.6667 15.3333C17.0009 15.3333 16.216 15.2167 15.5686 15.3901C14.9935 15.5442 14.5442 15.9935 14.3901 16.5686C14.2167 17.216 14.3333 18.0009 14.3333 18.6667C14.3333 20.5076 12.8409 22 11 22"></path>
+                              <path stroke-linecap="round" stroke-width="2" stroke="#171718" d="M11 6L3 6M7 2V10"></path>
+                            </svg>`;
 
-    const actionsCell = document.createElement('td');
-    const copyButton = document.createElement('button');
-    copyButton.textContent = 'Copy';
-    copyButton.addEventListener('click', () => {
-        copyTemplate(template.content);
-    });
-    actionsCell.appendChild(copyButton);
+  // Create the description element
+  const descElement = document.createElement('div');
+  descElement.classList.add('desc');
 
-    newRow.appendChild(titleCell);
-    newRow.appendChild(contentCell);
-    newRow.appendChild(actionsCell);
+  const titleLabel = document.createElement('label');
+  titleLabel.textContent = template.title;
 
-    tableBody.appendChild(newRow);
+  const contentSpan = document.createElement('span');
+  contentSpan.textContent = template.content;
+
+  descElement.appendChild(titleLabel);
+  descElement.appendChild(contentSpan);
+
+  // Create the badge button element
+  const badgeButton = document.createElement('button');
+  badgeButton.classList.add('badge');
+  badgeButton.textContent = 'Copy';
+  badgeButton.addEventListener('click', () => {
+    copyTemplate(template.content);
+  });
+
+  // Append the elements to the template element
+  templateElement.appendChild(iconElement);
+  templateElement.appendChild(descElement);
+  templateElement.appendChild(badgeButton);
+
+  // Append the template element to the results container
+  resultsContainer.appendChild(templateElement);
 }
 
-// Function to populate the table with templates
-function populateTable(templates) {
-    const tableBody = document.querySelector('table tbody');
-
-    templates.forEach((template) => {
-        const newRow = document.createElement('tr');
-
-        const titleCell = document.createElement('td');
-        titleCell.textContent = template.title;
-
-        const contentCell = document.createElement('td');
-        contentCell.textContent = template.content;
-
-        const actionsCell = document.createElement('td');
-        const copyButton = document.createElement('button');
-        copyButton.textContent = 'Copy';
-        copyButton.addEventListener('click', () => {
-            copyTemplate(template.content);
-        });
-        actionsCell.appendChild(copyButton);
-
-        newRow.appendChild(titleCell);
-        newRow.appendChild(contentCell);
-        newRow.appendChild(actionsCell);
-
-        tableBody.appendChild(newRow);
-    });
+function loadTemplatesFromJSON() {
+  fetch('./templates/template.json')
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((template) => {
+        if (template.default) {
+          // Add the template as a default template
+          addDefaultTemplate(template);
+        } else {
+          // Add the template as a custom template
+          saveTemplate(template);
+        }
+      });
+    })
+    .catch((error) => console.error('Failed to load templates from JSON:', error));
 }
 
-// Function to load templates from template.json
-function loadTemplatesFromJSON(callback) {
-    fetch('./templates/template.json')
-        .then((response) => response.json())
-        .then((data) => callback(data))
-        .catch((error) => console.error('Failed to load templates from JSON:', error));
+function addDefaultTemplate(template) {
+  // Add the template to the results container
+  const resultsContainer = document.getElementById('resultsContainer');
+
+  // Create the template element
+  const templateElement = document.createElement('div');
+  templateElement.classList.add('entry');
+
+  // Create the icon element
+  const iconElement = document.createElement('div');
+  iconElement.classList.add('icon');
+  iconElement.innerHTML = `<svg fill="none" viewBox="0 0 24 24" height="20" width="20">
+                              <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2" stroke="#171718" d="M13 2H13.2727C16.5339 2 18.1645 2 19.2969 2.79784C19.6214 3.02643 19.9094 3.29752 20.1523 3.60289C21 4.66867 21 6.20336 21 9.27273V11.8182C21 14.7814 21 16.2629 20.5311 17.4462C19.7772 19.3486 18.1829 20.8491 16.1616 21.5586C14.9044 22 13.3302 22 10.1818 22C8.38275 22 7.48322 22 6.76478 21.7478C5.60979 21.3424 4.69875 20.4849 4.26796 19.3979C4 18.7217 4 17.8751 4 16.1818V12"></path>
+                              <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2" stroke="#171718" d="M21 12C21 13.8409 19.5076 15.3333 17.6667 15.3333C17.0009 15.3333 16.216 15.2167 15.5686 15.3901C14.9935 15.5442 14.5442 15.9935 14.3901 16.5686C14.2167 17.216 14.3333 18.0009 14.3333 18.6667C14.3333 20.5076 12.8409 22 11 22"></path>
+                              <path stroke-linecap="round" stroke-width="2" stroke="#171718" d="M11 6L3 6M7 2V10"></path>
+                            </svg>`;
+
+  // Create the description element
+  const descElement = document.createElement('div');
+  descElement.classList.add('desc');
+
+  const titleLabel = document.createElement('label');
+  titleLabel.textContent = template.title;
+
+  const contentSpan = document.createElement('span');
+  contentSpan.textContent = template.content;
+
+  descElement.appendChild(titleLabel);
+  descElement.appendChild(contentSpan);
+
+  // Create the badge button element
+  const badgeButton = document.createElement('button');
+  badgeButton.classList.add('badge');
+  badgeButton.textContent = 'Copy';
+  badgeButton.addEventListener('click', () => {
+    copyTemplate(template.content);
+  });
+
+  // Append the elements to the template element
+  templateElement.appendChild(iconElement);
+  templateElement.appendChild(descElement);
+  templateElement.appendChild(badgeButton);
+
+  // Append the template element to the results container
+  resultsContainer.appendChild(templateElement);
+}
+
+function handleSetToDefault() {
+  const resultsContainer = document.getElementById('resultsContainer');
+
+  // Get all the custom-made templates (entries with 'custom' class)
+  const customEntries = resultsContainer.getElementsByClassName('custom');
+
+  // Remove the custom-made templates
+  while (customEntries.length > 0) {
+    const entry = customEntries[0];
+    entry.remove();
+  }
 }
 
 // Load existing templates on page load
 document.addEventListener('DOMContentLoaded', () => {
+  // Load templates from the JSON file and populate the results container
+  loadTemplatesFromJSON();
 
+  // Add event listener to the form for template submission
+  const templateForm = document.getElementById('templateForm');
+  templateForm.addEventListener('submit', handleFormSubmit);
 
-    loadTemplatesFromJSON((templates) => {
-        populateTable(templates);
-    });
+  // Add event listener to the "Set to Default" button
+  const setToDefaultButton = document.getElementById('setToDefaultButton');
+  setToDefaultButton.addEventListener('click', handleSetToDefault);
 });
 
-// Add event listener to the form for template submission
-const templateForm = document.getElementById('templateForm');
-templateForm.addEventListener('submit', handleFormSubmit);
+function handleSetToDefault() {
+  const resultsContainer = document.getElementById('resultsContainer');
 
-// Function to set the table to default state
-function setToDefault() {
-    loadTemplatesFromJSON((templates) => {
-      const tableBody = document.querySelector('table tbody');
-      tableBody.innerHTML = '';
-      populateTable(templates);
-    });
+  // Get all the custom-made templates (entries with 'custom' class)
+  const customEntries = resultsContainer.getElementsByClassName('custom');
+
+  // Remove the custom-made templates
+  while (customEntries.length > 0) {
+    const entry = customEntries[0];
+    entry.remove();
   }
+}
 
-// Add event listener to the "Set to Default" button
-const setToDefaultButton = document.getElementById('setToDefaultButton');
-setToDefaultButton.addEventListener('click', setToDefault);
+// Function to display the current clipboard content
+function displayClipboardContent() {
+  navigator.clipboard.readText()
+    .then((text) => {
+      console.log('Current clipboard content:', text);
+    })
+    .catch((error) => {
+      console.error('Failed to read clipboard content:', error);
+    });
+}
+
+// Add event listener to the "Display Clipboard" button
+const displayClipboardButton = document.getElementById('displayClipboardButton');
+displayClipboardButton.addEventListener('click', displayClipboardContent);
